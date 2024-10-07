f12383ae6e77a7ea5e00c952108c3b2201b6dfbe
Gregorio Palamà <gregoriopalama@gmail.com>
2024-10-07 10:22:57 +0200

    fixes password checks (#3)

--- db/02_seed.sql
+++ db/02_seed.sql
@@ -2,9 +2,9 @@ USE image_catalog;
 
 -- Popolamento tabella users con un amministratore e alcuni utenti normali
 INSERT INTO users (username, password, role) VALUES
-('admin', '$2y$10$Ew1S/oBgOzddwzD2hLT0t.8Oofh1E.H9RuQ4YjGb6lgT0Gh5G2ftK', 'admin'),  -- Password: admin123
-('user1', '$2y$10$e1zUV7EvFQb/VqjUS1U15O04kDXdzboF4AGU.yPvgHSvmzgV6hg16', 'user'),   -- Password: user123
-('user2', '$2y$10$yfU8F5v/OAVYoZz2EB6gNOozF.SAqA.VduOASCN5UuMJPmq41E7be', 'user');   -- Password: user123
+('admin', '$2y$10$il9iJULRoOJWqY4L9PmOGeL32WJnLdPYWjtWtS3KnxWBXxWK6dQnG', 'admin'),  -- Password: admin123
+('user1', '$2y$10$UE4I6iAbZ5KvadGShTJMcelhb9s5DC0drsjC4wElkcrRZf0.BUMgW', 'user'),   -- Password: user123
+('user2', '$2y$10$hGhR9fA4Qb5v0dgVpfoLOuKz9EQxTwSjJvTWhVcwlTvc5K/kLpFdO', 'user');   -- Password: user123
 
 -- Popolamento tabella images con alcune immagini iniziali
 INSERT INTO images (user_id, filename, inappropriate) VALUES
