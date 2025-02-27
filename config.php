ee0f50e5e02878e6f630caf1447c63047ee9102a
mathieu <delphi.mat@gmail.com>
2025-02-27 15:08:05 +0100

    push docker file

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.154.4.7';
 $db_name = 'image_catalog';
 $db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_pass = 'CLOUDSQL_INSTANCE_PASSWORD';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
